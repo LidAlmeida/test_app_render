cef962948eb7ea94ce956ff7be675e5baadd4d77
Lidi <lidi.calmeida@gmail.com>
2023-06-13 22:13:47 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -44,8 +44,7 @@ def rossmann_predict():
 
     else:
         return Response('{}', status=200, mimetype='application/json')
-
-
+    
 if __name__ == '__main__':
-    os.environ.get('PORT', 5000)
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
