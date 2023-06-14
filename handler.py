d7715f38e13456d546df791e2063ca8ff7633a08
Lidi <lidi.calmeida@gmail.com>
2023-06-14 15:23:11 -0300

    update handler

--- handler.py
+++ handler.py
@@ -11,11 +11,6 @@ model = pickle.load(open('model/pipe.pkl', 'rb'))
 # initialize app
 app = Flask( __name__ )
 
-@app.route('/', methods=['GET'])
-def home():
-    return "Welcome to the Rossmann API!"
-
-
 @app.route('/rossmann/predict', methods=['POST'])
 def rossmann_predict():
     test_json = request.get_json()
